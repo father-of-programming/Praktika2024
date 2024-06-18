3fc6f2cfc0b9b36e8a13d91a453955d8b14cb55e
Vladimir Baryshnikov <megabariga2915@gmail.com>
2024-06-18 13:01:19 +0700

    Создание главного окна, его открытие и доп окна с выбором начального количества "цариц". Кнопка "Назад" в доп окне не работает

--- src/menu/menuWnd.java
+++ src/menu/menuWnd.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 public class menuWnd extends Application{
@@ -11,9 +12,9 @@ public class menuWnd extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("menuWnd.fxml"));
-        stage.setTitle("TBG");
-        //stage.setFullScreenExitHint("");    //убираем сообщение о выходе из полноэкр режима
-        stage.setScene(new Scene(root,700, 390));
+        stage.setTitle("Colony");
+        stage.setMaximized(true);
+        stage.setScene(new Scene(root,1920, 1080));
         stage.show();
     }
 
--- src/menu/menuWndCntrl.java
+++ src/menu/menuWndCntrl.java
@@ -9,8 +9,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.swing.text.html.ImageView;
 
@@ -28,18 +30,6 @@ public class menuWndCntrl {
     @FXML
     private Button newcolonyBtn;
 
-    public void handlenewcolonyBtn(ActionEvent actionEvent) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("../main/mainWnd.fxml"));
-
-        Scene scene = new Scene(root);
-        Stage newstage = new Stage();
-        newstage.setTitle("New wnd");
-        newstage.setScene(scene);
-        newstage.initModality(Modality.WINDOW_MODAL);
-        //newstage.initOwner(newcolonyBtn.getScene().getWindow());
-        newstage.show();
-    }
-
     @FXML
     private Button statsBtn;
 
@@ -50,6 +40,29 @@ public class menuWndCntrl {
     void initialize() {
         exitBtn.setOnAction(actionEvent -> {
             System.exit(0);
+        });
+
+        newcolonyBtn.setOnAction(actionEvent -> {
+            newcolonyBtn.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/main/caricaWnd.fxml"));
+            try{
+                loader.load();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Муравьиная колония");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.showAndWait();
+        });
+
+        statsBtn.setOnAction(actionEvent -> {
+            System.out.println("stats");
         });
     }
 
